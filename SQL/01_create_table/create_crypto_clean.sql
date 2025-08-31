91a9e6170a50c3b8661818123ec4f5eec30ddbcf
Jesse Rai <Jesserai2012@me.com>
2025-08-31 01:20:07 -0700

    Add create_crypto_clean.sql script

---
+++ SQL/01_create_table/create_crypto_clean.sql
@@ -0,0 +1,10 @@
+CREATE TABLE crypto_clean (
+  Currency    TEXT,
+  date        DATE,
+  Open        NUMERIC,
+  High        NUMERIC,
+  Low         NUMERIC,
+  Close       NUMERIC,
+  Volume      NUMERIC,
+  "Market Cap"  NUMERIC
+);
